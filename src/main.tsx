84254a5256ac7ae891146d974f9ea0cb40ca9d58
ibr7h <d.alneami@gmail.com>
2026-09-13 18:24:10 +0300

    Initialize native Android prayer scheduler

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { startNativePrayerScheduler } from './nativeScheduler';
 import './style.css';
+
+startNativePrayerScheduler();
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
